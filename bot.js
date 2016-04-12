d81b6ed662fab6f155656130305b1e04b4ea8cba
Axel Eirola <axel.eirola@futurice.com>
2016-04-12 11:52:48 +0300

    Add language analysis

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('lodash');
-var yargs = require('yargs');
+var axios = require('axios');
 
 var HELP = '# Help\n' +
   '- `help`\n' +
@@ -10,6 +10,17 @@ var HELP = '# Help\n' +
   '- `toc`';
 
 var SLIDES = [];
+
+var ANALYSIS_THRESHOLD = 0.4;
+
+var RESPONSES = {
+  HATEFULL_INPUT: [
+    'Don\'t talk to me like that.',
+    'Watch your language.',
+    'I don\'t need to listen to this kind of talk.',
+    'I\'m a self-respecting bot, not some YouTube commenter.'
+  ]
+};
 
 var SLIDES_PATH = path.join(__dirname, 'slides');
 fs.readdir(SLIDES_PATH, function(err, files) {
@@ -32,6 +43,46 @@ fs.readdir(SLIDES_PATH, function(err, files) {
     });
   })
 });
+
+function preprocess(session) {
+  var inputText = session.message.text;
+  console.log('Preprocessing input:', inputText);
+
+  return axios({
+    method: 'POST',
+    url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
+    headers: {
+      'Ocp-Apim-Subscription-Key': '6e4beeafe284496191c9f55dc7417ee2'
+    },
+    data: {
+      documents: [{
+        id: "input",
+        text: inputText
+      }]
+    }
+  }).catch(function(err) {
+    console.error('Linguistic analysis failed:', err);
+
+    return {
+      score: null,
+      text: inputText
+    };
+  }).then(function(response) {
+    var score = _.get(response, 'data.documents[0].score', 0.5);
+    console.log('Text analysis complete:', score);
+
+    if (score < ANALYSIS_THRESHOLD) {
+      session.send(_.sample(RESPONSES.HATEFULL_INPUT));
+      throw new Error('Hatefull speech error (' + score +  ')');
+      return;
+    }
+
+    return {
+      score: score,
+      text: inputText
+    };
+  });
+}
 
 function sendHelp(session) {
   return session.send(HELP);
@@ -71,25 +122,33 @@ function sendPreviousSlide(session) {
   return session.send(SLIDES[slideIndex].content);
 }
 
+function sendReply(session) {
+  var input = session.message.text;
+
+  switch (input.toLowerCase()) {
+    case 'help':
+      return sendHelp(session);
+    case 'topics':
+      return sendTopics(session);
+    case 'next':
+      return sendNextSlide(session);
+    case 'prev':
+      return sendPreviousSlide(session);
+  }
+
+  if (_.startsWith(input.toLowerCase(), 'slide ')) {
+    return sendSlide(session, input.toLowerCase().replace(/^slide /, ''));
+  }
+
+  return session.send('What do you mean?');
+}
+
 module.exports = function addDialogs(bot) {
   bot.add('/', function (session) {
-    var input = session.message.text;
-
-    switch (input.toLowerCase()) {
-      case 'help':
-        return sendHelp(session);
-      case 'topics':
-        return sendTopics(session);
-      case 'next':
-        return sendNextSlide(session);
-      case 'prev':
-        return sendPreviousSlide(session);
-    }
-
-    if (_.startsWith(input.toLowerCase(), 'slide ')) {
-      return sendSlide(session, input.toLowerCase().replace(/^slide /, ''));
-    }
-
-    return session.send('What do you mean?');
+    preprocess(session).then(function(processed) {
+      sendReply(session);
+    }).catch(function(e) {
+      console.error(e);
+    });
   });
 };
